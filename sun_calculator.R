69c3621a905476add49268e63ccf41049dc9c038
Drew Morris <baltimorris@gmail.com>
2026-02-10 18:59:07 -0500

    added logic to calculate example rays based on provided azimuth, next i need to function it to apply to every az value

--- sun_calculator.R
+++ sun_calculator.R
@@ -1,4 +1,10 @@
 library(suncalc)
+library(leaflet)
+library(dplyr)
+library(tidyverse)
+library(sf)
+
+#dc lat/lon, roughly Washington Cir NW
 lat <- 38.9073
 lon <- -77.0369
 
@@ -22,12 +28,26 @@ sun_df <- data.frame(
 sun_df$azimuth_deg <- (sun_df$azimuth_rad * 180/pi) + 180
 sun_df$elevation_deg <- sun_df$altitude_rad * 180/pi
 
-sun_df_day <- subset(sun_df, elevation_deg > 0)
+#drop nighttime
+sun_df_day <- sun_df %>%
+  filter(elevation_deg > 0)
 
-# Export CSV
+dist <- 10
+heading <- 124.65201
+lat2 <- ((dist/111.2) * cos(heading))+lat
+lon2 <- ((dist/111.2) * (sin(heading)/cos(lat)))+lon
+
+map <- leaflet() %>%
+  addTiles() %>%
+  setView(lng = lon, lat = lat, zoom= 15) %>%
+  addMarkers(lng = lon, lat = lat) %>%
+  addPolylines(lng = c(lon, lon2), lat = c(lat, lat2)) %>%
+  addCircleMarkers(lng = lon, lat = lat, radius = 50, stroke = TRUE, color = 'black', opacity = 1, fillColor = 'black', fillOpacity = 0.2) %>%
+  print(map)
+
+# Export CSV -- not necessary tbh mostly for arcpro use
 write.csv(sun_df_day,
           "C:\\Users\\E062583\\Documents\\sun_position_hourly_2026.csv",
           row.names = FALSE)
 
-head(sun_df)
 
